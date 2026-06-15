e56912a3f905f743ab4fdc2d78a8ae93a78a891e
Mohamed <mohamedwaleed2308@gmail.com>
2026-06-15 14:25:48 +0300

    production time

--- src/DB/DB.connection.js
+++ src/DB/DB.connection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const DBconnection = () => {
-    mongoose.connect("mongodb://localhost:27017/FinanceTracker").
+    mongoose.connect(process.env.DB_URL).
     then(() => {
             console.log('DB connected successfully')}).
     catch(()=>{
